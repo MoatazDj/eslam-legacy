3206a71055c522c3ed8fe469e2cf12291ca46089
Bouabda <abdaoui.wala@gmail.com>
2020-05-14 01:03:35 +0100

    navbar and home components added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    FooterComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
