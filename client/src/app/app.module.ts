801d80b04e50b68da3744ec3231984a37b06bb6f
Bouabda <abdaoui.wala@gmail.com>
2020-05-14 18:18:55 +0100

    most of the components created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { FooterComponent } from './footer/footer.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarModule, IslamicService } from '@syncfusion/ej2-angular-calendars';
+import { VersesComponent } from './verses/verses.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { StatesComponent } from './states/states.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +22,13 @@ import { CalendarModule, IslamicService } from '@syncfusion/ej2-angular-calendar
     HomeComponent,
     AboutUsComponent,
     FooterComponent,
-    CalendarComponent
+    CalendarComponent,
+    VersesComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    StatesComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -15,31 +15,7 @@ export class CalendarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onLoad(args: any) {
-    /*Date need to be disabled*/
-    if (args.date.getDate() === 12 || args.date.getDate() === 17 || args.date.getDate() === 22) {
-       args.isDisabled = true;
-   }
-   /*Dates need to be customized*/
-   if (args.date.getDate() === 13) {
-       let span: HTMLElement;
-       span = document.createElement('span');
-       args.element.children[0].className += 'e-day sf-icon-cup highlight';
-       addClass([args.element], ['special', 'e-day', 'dinner']);
-       args.element.setAttribute('data-val', ' Dinner !');
-       args.element.appendChild(span);
-     }
-     if (args.date.getDate() === 23) {
-       let span: HTMLElement;
-       span = document.createElement('span');
-       args.element.children[0].className += 'e-day sf-icon-start highlight';
-       span.setAttribute('class', 'sx !');
-       //use the imported method to add the multiple classes to the given element
-       addClass([args.element], ['special', 'e-day', 'holiday']);
-       args.element.setAttribute('data-val', ' Holiday !');
-       args.element.appendChild(span);
-     }
-} 
+  
 }
 
 
