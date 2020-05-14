2d833452e4c94f0c4423b53990317af3a6448d4d
Amir Ben Youssef <amirbenyoussef3@gmail.com>
2020-05-14 02:49:22 +0100

    finish server side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-=======
-import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> aac0b656d0834007870bb41603bc7116c4f95850
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
 export class AppComponent {
   title = 'client';
-=======
-
-export class AppComponent {
-  
->>>>>>> aac0b656d0834007870bb41603bc7116c4f95850
 }
