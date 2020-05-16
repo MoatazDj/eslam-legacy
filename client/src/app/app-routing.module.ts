16a363bc6c4d9a0b3d5e0a94d649faafaea7c7c3
MootezDj <djebalimootez1@gmail.com>
2020-05-16 04:23:12 +0300

    sending the verses

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {path:'login',component: LoginComponent},
   {path:'register',component: RegisterComponent},
   {path:'profile',component: ProfileComponent},
-  {path:'favourites',component: FavoritesComponent},
+  {path:'favorites',component: FavoritesComponent},
 ];
 
 @NgModule({
