bdfe7af8273bd81e297cc73387204f0c8575cf54
Amir Ben Youssef <amirbenyoussef3@gmail.com>
2020-05-18 00:30:03 +0100

    finshing login signup log out, donate

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
 module.exports.auth = require('./auth');
 module.exports.verses = require('./verses');
 module.exports.favourites = require('./favourites');
+module.exports.paypal = require('./paypal');
